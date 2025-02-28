4aed0a81146a32e82ab24eb1c64e60bee786a7ff
Pranitha2005-tech <pranichinni25@gmail.com>
2025-02-28 12:56:09 +0530

    pranitha.12

--- code.py
+++ code.py
@@ -1,6 +1,6 @@
-# Function to subtract two numbers
-def subtract_numbers(num1, num2):
-    return num1 - num2
+# Function to subtract three numbers
+def subtract_numbers(num1, num2 , num3):
+    return num1 - num2 - num3
 
 # Input from the user
 num1 = float(input("Enter the first number: "))
